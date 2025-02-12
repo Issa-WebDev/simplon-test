91dd64f75158de079ae6802abeed596c247c8757
Issa-WebDev <kissadev10@gmail.com>
2025-02-12 19:14:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const toggleBtn = document.getElementById("toggleBtn");
+const btnMessage = document.getElementById("btnMessage");
+const message = document.getElementById("message");
+
+// toggle darkMode
+let theme = false;
+toggleBtn.addEventListener("click", toggleFunc);
+
+function toggleFunc() {
+  theme = !theme;
+  document.body.classList.toggle("darkMode", theme);
+
+  theme ? (toggleBtn.style.color = "#000") : (toggleBtn.style.color = "#fff");
+}
+
+// message
+btnMessage.addEventListener("click", () => {
+  document.querySelector(".text").style.display = "none";
+  btnMessage.style.display = "none";
+  const strMessage = "Merci💘❤️d'avoir visiter ma page personnelle!";
+  let index = 0;
+  const intervalId = setInterval(() => {
+    message.textContent = strMessage.slice(0, index++);
+
+    if (index > message.length) clearInterval(intervalId);
+  }, 100);
+});
+
+
+
+
